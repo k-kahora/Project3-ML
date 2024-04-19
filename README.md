4c49ac2620e7659b322af8eba05cef6781c72bee
k-kahora <mango22mjk@gmail.com>
2024-04-19 16:38:52 +0000

    New addition and updated readme

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ cat out.txt
 To run all three at once
 
 ``` python
-./runnner.sh
+./runner.sh
 ```
 
 To observe the output 
@@ -26,3 +26,7 @@ cat D3
 cat D4
 ```
 
+
+# Files
+
+__D2__, __D3__, __D4__ are all the output decision trees from running the algorithm on the three data sets.  __PlayTennisCustom.txt__ is the data set for __D4__, __D3__ is the tree for __PlayTennisSampleDataFormat.txt__, and __D1__ is the tree for __EnjoySport.txt__.  __D3-answer.txt__ is the answer for the question on Task3.  __program.py__ is the main program that runs ID3, __runner.sh__ runs all three datasets at once and overwrites __D1__, __D2__, and __D3__.
--- program.py
+++ program.py
@@ -6,14 +6,14 @@ import sys
 import csv
 from math import log2
 
-# Target attribute is always the attribute with the target value
+# The target attribute is always the attribute with the target value
 def entropy(subset, target_attribute):
     total = len(subset)
     if total == 0:
         return 0
-    class_counts = {val: 0 for val in set(item[target_attribute] for item in subset)} # Creates a dictionary of possible values 
+    class_counts = {val: 0 for val in set(item[target_attribute] for item in subset)} # Creates a dictionary of possible values, begin Yes or No in this case.
     for item in subset:
-        class_counts[item[target_attribute]] += 1 # counts up the amount for each
+        class_counts[item[target_attribute]] += 1 # Counts up the amount for each
     return sum((-count/total) * log2(count/total) for count in class_counts.values() if count > 0)
 
 def info_gain(data, attribute, target_attribute):
@@ -26,18 +26,18 @@ def info_gain(data, attribute, target_attribute):
     return total_entropy - weighted_entropy
 
 def id3(data, attributes, target_attribute):
-    target_values = set(item[target_attribute] for item in data) # value could be on or two
+    target_values = set(item[target_attribute] for item in data) # Value could be one or two
     if len(target_values) == 1:
         return next(iter(target_values))  # All examples are the same class so just return the class
     if not attributes:
         most_common = max(target_values, key=lambda val: sum(item[target_attribute] == val for item in data))
         return most_common
-    best_attribute = max(attributes, key=lambda attr: info_gain(data, attr, target_attribute)) # This is the *best attribute or the attribute to split on for the most information gain
+    best_attribute = max(attributes, key=lambda attr: info_gain(data, attr, target_attribute)) # This is the best attribute or the attribute to split on for the most information gain
     tree = {best_attribute: {}}
-    remaining_attributes = [attr for attr in attributes if attr != best_attribute] # remove the best attribute
-    for value in set(item[best_attribute] for item in data): # Loop through only the best attribute for each data item 
-        subset = [item for item in data if item[best_attribute] == value] # This is all data items that have the same current value thats iterated from the best attribute
-        subtree = id3(subset, remaining_attributes, target_attribute) # get the best subtree from the remaining subset
+    remaining_attributes = [attr for attr in attributes if attr != best_attribute] # Remove the best attribute
+    for value in set(item[best_attribute] for item in data): # Loop through only the best attribute for each data item
+        subset = [item for item in data if item[best_attribute] == value] # This is all data items that have the same current value that's iterated from the best attribute
+        subtree = id3(subset, remaining_attributes, target_attribute) # Get the best subtree from the remaining subset
         tree[best_attribute][value] = subtree
     return tree
 
@@ -47,12 +47,12 @@ def read_data(filepath):
         data = list(reader)
         return data
 
-# Pretty print the subtree so that is is easily readable
+# Pretty print the subtree so that it is easily readable
 def print_tree(tree, indent="", file=sys.stdout):
-    if isinstance(tree, dict): # Need to check If the tree is a single value yes or no 
+    if isinstance(tree, dict): # Need to check if the tree is a single value yes or no
         for key, val in tree.items():
             print(indent + str(key) + ":", file=file)
-            if isinstance(val, dict): # Could be a tree or a sinlge value yes or no
+            if isinstance(val, dict): # Could be a tree or a single value yes or no
                 print_tree(val, indent + "\t", file=file)
             else:
                 print(indent + "\t" + str(val), file=file)
@@ -73,7 +73,7 @@ def main():
     if 'ExampleID' in attributes:
         attributes.remove('ExampleID')  # Remove ExampleID from attributes as it's not used
 
-    # Data is formatted and in expected positons, unneccesary attributes have been removed
+    # Data is formatted and in expected positions, unnecessary attributes have been removed
     decision_tree = id3(data, attributes, target_attribute)
     
     with open(output_filepath, 'w') as file:
